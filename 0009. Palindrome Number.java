ec1113eeb518cb80318a1d676f9fdce55047111f
Zeyu Ni <joey.ni827@gmail.com>
2020-04-07 12:42:30 -0700

    Create 0009. Palindrome Number.java

---
+++ 0009. Palindrome Number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        int y = reverse(x);
+        
+        if (x == y) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public int reverse(int x) {
+        int m = Math.abs(x);
+        
+        long result = 0;
+        while(m > 0) {
+            result = result * 10 + m % 10;
+            if(result > Integer.MAX_VALUE) {
+                return 0;
+            }
+            m /= 10;
+        }
+        
+        if(x < 0) {
+            return -(int)result;
+        }
+        
+        return (int)result;
+    }
+}
