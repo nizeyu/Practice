0f53dae853019158bb3e4f57e474f1afa5492431
Zeyu Ni <joey.ni827@gmail.com>
2020-04-07 18:35:02 -0700

    Create 0038. Count and Say.java

---
+++ 0038. Count and Say.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        
+        String strs = countAndSay(n - 1) + "#";
+        StringBuilder sb = new StringBuilder();
+        
+        int count = 1;
+        for (int i = 0; i < strs.length() - 1; i++) {
+            if (strs.charAt(i) != strs.charAt(i + 1)) {
+                sb.append(count);
+                sb.append(strs.charAt(i));
+                count = 1;
+            } else {
+                count++;
+            } 
+        }
+        
+        return sb.toString();
+    }
+}
