39c16d7d719d4c684d9e6661a95e34e1523dc950
Zeyu Ni <joey.ni827@gmail.com>
2020-04-08 10:20:30 -0700

    Create 0088. Merge Sorted Array.java

---
+++ 0088. Merge Sorted Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {    
+        int a = m - 1;
+        int b = n - 1;
+        int i = m + n - 1;
+        while (a >= 0 && b >= 0) {
+            if (nums1[a] >= nums2[b]) {
+                nums1[i] = nums1[a];
+                a--;
+            } else {
+                nums1[i] = nums2[b];
+                b--;
+            }
+            i--;
+        }
+        
+        if (b >= 0) {
+            while(i >= 0 && b >= 0) {
+                nums1[i] = nums2[b];
+                i--;
+                b--;
+            }
+        }
+    }
+}
