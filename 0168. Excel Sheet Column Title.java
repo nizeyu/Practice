146e8e50664e0cb54f25897aa54779175558947d
Zeyu Ni <joey.ni827@gmail.com>
2020-04-08 18:05:58 -0700

    Create 0168. Excel Sheet Column Title.java

---
+++ 0168. Excel Sheet Column Title.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToTitle(int n) {
+        if (n < 1) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            n--;
+            sb.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+        
+        return sb.toString();
+    }
+}
