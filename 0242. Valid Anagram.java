0dbc282ab13c3eae29a32ab77000a8f99f61fc62
Zeyu Ni <joey.ni827@gmail.com>
2020-04-09 15:03:00 -0700

    Create 0242. Valid Anagram.java

---
+++ 0242. Valid Anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] map = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+        }
+        
+        for (int i = 0; i < t.length(); i++) {
+            if (map[t.charAt(i)] <= 0) {
+                return false;
+            } else {
+                map[t.charAt(i)]--;
+            }
+        }
+        
+        return true;
+    }
+}
