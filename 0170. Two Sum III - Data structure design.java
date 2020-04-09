af2a2cdbf3b95c1a8f53999c71f33ba887d2f00e
Zeyu Ni <joey.ni827@gmail.com>
2020-04-08 18:37:12 -0700

    Create 0170. Two Sum III - Data structure design.java

---
+++ 0170. Two Sum III - Data structure design.java
@@ -0,0 +1,37 @@
+class TwoSum {
+    
+    Map<Integer, Integer> map;
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int num : map.keySet()) {
+            int num1 = value - num;
+            
+            if (num == num1 && map.get(num) > 1) {
+                return true;
+            }
+            
+            if (num != num1 && map.containsKey(num1)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
