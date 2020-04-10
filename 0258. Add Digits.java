e1e45c7bf21a23d790b6322eb2585d8a0b38f353
Zeyu Ni <joey.ni827@gmail.com>
2020-04-09 18:16:29 -0700

    Create 0258. Add Digits.java

---
+++ 0258. Add Digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        while (true) {
+            int sum = 0;
+            while (num > 0) {
+                sum  += num % 10;
+                num /= 10;
+            }
+            
+            if (sum < 10) {
+                return sum;
+            }
+            
+            num = sum;
+        }
+    }
+}
