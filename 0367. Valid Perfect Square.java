cd547c96e46f62400d9ebc47a290e50ed62bb4ba
Zeyu Ni <joey.ni827@gmail.com>
2020-04-10 17:58:28 -0700

    Create 0367. Valid Perfect Square.java

---
+++ 0367. Valid Perfect Square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int l = 1;
+        int r = num;
+        
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            
+            if (num == m * m) {
+                return true;
+            }
+            
+            if (num / m <= m) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        
+        return num == l * l;
+    }
+}
