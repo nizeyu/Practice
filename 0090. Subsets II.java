183bf08a39a165b0cf42bf3e92d9c2405bb6202f
Zeyu Ni <joey.ni827@gmail.com>
2020-04-14 09:58:57 -0700

    Create 0090. Subsets II.java

---
+++ 0090. Subsets II.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+        
+        if (nums.length == 0) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+        
+        Arrays.sort(nums);
+        
+        dfs(0, new ArrayList<>(), nums, result);
+        
+        return result;
+    }
+    
+    private void dfs(int pos, List<Integer> subset, int[] nums, List<List<Integer>> result) {
+        result.add(new ArrayList<>(subset));
+        
+        for (int i = pos; i < nums.length; i++) {
+            if (i != pos && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            subset.add(nums[i]);
+            dfs(i + 1, subset, nums, result);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
