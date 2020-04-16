7b2371d2abd52199732c767520788a222564dc15
Zeyu Ni <joey.ni827@gmail.com>
2020-04-16 09:36:37 -0700

    Update 0367. Valid Perfect Square.java

--- 0367. Valid Perfect Square.java
+++ 0367. Valid Perfect Square.java
@@ -10,7 +10,7 @@ class Solution {
                 return true;
             }
             
-            if (num / m <= m) {
+            if (num / m < m) {
                 r = m;
             } else {
                 l = m + 1;
