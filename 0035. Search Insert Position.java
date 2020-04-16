7f639baa30a5601a7b6c023bf542ecb76af33919
Zeyu Ni <joey.ni827@gmail.com>
2020-04-16 15:51:57 -0700

    Update 0035. Search Insert Position.java

--- 0035. Search Insert Position.java
+++ 0035. Search Insert Position.java
@@ -4,6 +4,38 @@ class Solution {
             return -1;
         }
         
+        int l = 0;
+        int r = nums.length - 1;
+        while(l + 1 < r) {
+            int mid = l + (r - l) / 2;
+                        
+            if (target == nums[mid]) {
+                r = mid; 
+            } else if (target < nums[mid]) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        
+        if (nums[l] >= target) { 
+            return l;
+        }
+        
+        if (nums[r] >= target) {
+            return r;
+        }
+        
+        return nums.length;
+    }
+}
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
         return lowerBound(nums, target);
     }
     
