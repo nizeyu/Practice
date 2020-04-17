b3ea90ec98fb53b6d5df1ec17a9ead19116e9ffe
Zeyu Ni <joey.ni827@gmail.com>
2020-04-17 07:41:16 -0700

    Create 0031. Next Permutation.java

---
+++ 0031. Next Permutation.java
@@ -0,0 +1,50 @@
+class Solution {
+    // Time: O(n)
+    // Space: O(1)
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        
+        int firstSmall = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i+1]) {
+                firstSmall = i;
+                break;
+            }
+        }
+        
+        if (firstSmall == -1) {
+            reverseArray(0, nums.length - 1, nums);
+        }
+        
+        int firstLarge = -1;
+        for (int i = nums.length - 1; i > firstSmall; i--) {
+            if (nums[i] > nums[firstSmall]) {
+                firstLarge = i;
+            }
+            break;
+        }
+        
+        swap(firstSmall, firstLarge, nums);
+        
+        reverseArray(firstSmall + 1, nums.length - 1, nums);
+    }
+    
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverseArray(int l, int r, int[] nums) {
+        while(l < r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            
+            l++;
+            r--;
+        }
+    }
+}
