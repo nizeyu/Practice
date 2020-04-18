58d8d297ef931418cd34a240fefcb27f30949c85
Zeyu Ni <joey.ni827@gmail.com>
2020-04-18 16:32:17 -0700

    Create 0844. Backspace String Compare.java

---
+++ 0844. Backspace String Compare.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        if (S == null || T == null) {
+            return false;
+        }
+        
+        int i = S.length() - 1, j = T.length() - 1;
+        int s = 0, t = 0;
+        
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    s++;
+                    i--;
+                } else {
+                    if (s > 0) {
+                        s--;
+                        i--;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    t++;
+                    j--;
+                } else {
+                    if (t > 0) {
+                        t--;
+                        j--;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            
+            if (i < 0 || j < 0) {
+                return i < 0 && j < 0;
+            }
+            
+            if (S.charAt(i) != T.charAt(j)) {
+                return false;
+            }
+            
+            i--;
+            j--;
+        }
+        
+        return true;
+    }
+}
