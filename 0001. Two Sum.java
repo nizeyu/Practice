570f301b099b23bb08b4650c1e3527dcabd16782
Zeyu Ni <joey.ni827@gmail.com>
2020-04-18 05:30:02 -0700

    Update 0001. Two Sum.java

--- 0001. Two Sum.java
+++ 0001. Two Sum.java
@@ -1,3 +1,9 @@
+// 方法：HashMap
+// time: O(n)
+// space: O(n)
+// 暴力解法需要找到 nums 中两两元素的组合，复杂度为O(n^2)。hashmap的应用，把这个过程变成了one pass。
+// hashmap 既能记录下遍历过元素的value-index pair, 又能在O(1) 时间判断 target - nums[i] 的值是否存在。
+// 本题不能用 sort后双指针的方法，因为要求返回index，而排序后index 就乱了。
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
